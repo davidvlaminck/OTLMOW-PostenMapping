e13c9729a394f16f04313f58814e9a14f7651e47
davidvlaminck <davidvlaminck85@gmail.com>
2022-12-07 13:39:06 +0100

    range for numbers implemented

--- UnitTests/PostAssetFactoryTests.py
+++ UnitTests/PostAssetFactoryTests.py
@@ -15,19 +15,23 @@ class PostAssetFactoryTests(TestCase):
         mapping = factory.posten_mapping
         self.assertTrue('1001.30704' in mapping)
         self.assertTrue('1001.20111' in mapping)
+
         self.assertTrue('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Verkeersbordsteun'
                         in mapping['1001.30704'])
         self.assertTrue('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#RetroreflecterendVerkeersbord'
                         in mapping['1001.20111'])
+
         self.assertDictEqual({
             'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Verkeersbordsteun': {
                 'attributen':
                     [{'typeURI': 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Verkeersbordsteun.diameter',
-                      'dotnotation': 'diameter'}]}
+                      'dotnotation': 'diameter',
+                      'type': 'http://www.w3.org/2001/XMLSchema#decimal',
+                      'value': '114',
+                      'range': None}]}
         }, mapping['1001.30704'])
 
-
-    def test_create_assets_from_post_bord_steun_folie(self):
+    def test_create_assets_from_post_1001_20111(self):
         factory = self.set_up_factory()
         created_assets = factory.create_assets_from_post('1001.20111')
 
@@ -39,7 +43,54 @@ class PostAssetFactoryTests(TestCase):
         self.assertEqual(1, bord_count)
         self.assertEqual(1, steun_count)
 
-    def test_create_assets_from_post_steun(self):
+        bord = next((a for a in created_assets if a.typeURI ==
+                     'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#RetroreflecterendVerkeersbord'), None)
+
+        self.assertIsNotNone(bord)
+
+        with self.subTest('correct value, not raising ValueError'):
+            try:
+                bord.oppervlakte.waarde = 0.4
+                self.assertTrue(True)
+            except ValueError:
+                self.fail('ValueError raised')
+
+        with self.subTest('incorrect value, raising ValueError'):
+            with self.assertRaises(ValueError):
+                bord.oppervlakte.waarde = 0.6
+
+    def test_create_assets_from_post_1001_20131(self):
+        factory = self.set_up_factory()
+        created_assets = factory.create_assets_from_post('1001.20131')
+
+        folie_count = sum(1 for a in created_assets if
+                          a.typeURI == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#RetroreflecterendeFolie')
+        bord_count = sum(1 for a in created_assets if
+                         a.typeURI == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#RetroreflecterendVerkeersbord')
+        steun_count = sum(1 for a in created_assets if
+                          a.typeURI == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Verkeersbordsteun')
+
+        self.assertEqual(1, folie_count)
+        self.assertEqual(1, bord_count)
+        self.assertEqual(1, steun_count)
+
+        folie = next((a for a in created_assets if a.typeURI ==
+                     'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#RetroreflecterendeFolie'), None)
+
+        self.assertIsNotNone(folie)
+
+        with self.subTest('correct value, not raising ValueError'):
+            try:
+                folie.folietype = 'folietype-3a'
+                self.assertTrue(True)
+            except ValueError:
+                self.fail('ValueError raised')
+
+        with self.subTest('incorrect value, raising ValueError'):
+            with self.assertRaises(ValueError):
+                folie.folietype = 'folietype-1'
+
+    def test_create_assets_from_post_1001_30704(self):
         factory = self.set_up_factory()
         created_assets = factory.create_assets_from_post('1001.30704')
 
@@ -50,3 +101,30 @@ class PostAssetFactoryTests(TestCase):
         self.assertEqual(0, folie_count)
         self.assertEqual(0, bord_count)
         self.assertEqual(1, steun_count)
+
+        steun = next((a for a in created_assets if a.typeURI ==
+                     'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Verkeersbordsteun'), None)
+
+        self.assertIsNotNone(steun)
+        self.assertEqual(114.0, steun.diameter.waarde)
+
+    def test_split_range_str(self):
+        with self.subTest('x < 0.5'):
+            expected_list = [('st', 0.5)]
+            self.assertListEqual(expected_list, PostAssetFactory.split_numeric_range_str('x < 0.5'))
+
+        with self.subTest('x <= 0.5'):
+            expected_list = [('ste', 0.5)]
+            self.assertListEqual(expected_list, PostAssetFactory.split_numeric_range_str('x <= 0.5'))
+
+        with self.subTest('0.5 < x'):
+            expected_list = [('gt', 0.5)]
+            self.assertListEqual(expected_list, PostAssetFactory.split_numeric_range_str('0.5 < x'))
+
+        with self.subTest('0.5 <= x'):
+            expected_list = [('gte', 0.5)]
+            self.assertListEqual(expected_list, PostAssetFactory.split_numeric_range_str('0.5 <= x'))
+
+        with self.subTest('0.5 < x <= 1'):
+            expected_list = [('gt', 0.5), ('ste', 1)]
+            self.assertListEqual(expected_list, PostAssetFactory.split_numeric_range_str('0.5 < x <= 1'))
--- otlmow_postenmapping/PostAssetFactory.py
+++ otlmow_postenmapping/PostAssetFactory.py
@@ -2,11 +2,10 @@ import os
 from pathlib import Path
 from typing import List
 
-from otlmow_model.BaseClasses.OTLObject import OTLObject
-from otlmow_model.Classes.Onderdeel.RetroreflecterendeFolie import RetroreflecterendeFolie
-from otlmow_model.Classes.Onderdeel.RetroreflecterendVerkeersbord import RetroreflecterendVerkeersbord
-from otlmow_model.Classes.Onderdeel.Verkeersbordsteun import Verkeersbordsteun
 from otlmow_converter.AssetFactory import AssetFactory
+from otlmow_converter.DotnotationHelper import DotnotationHelper
+from otlmow_model.BaseClasses.FloatOrDecimalField import FloatOrDecimalField
+from otlmow_model.BaseClasses.OTLObject import OTLObject
 
 from otlmow_postenmapping.SQLDbReader import SQLDbReader
 
@@ -25,15 +24,91 @@ class PostAssetFactory:
         mapping = self.posten_mapping[post]
         created_assets = []
         for type_uri in mapping.keys():
-            created_assets.append(AssetFactory.dynamic_create_instance_from_uri(type_uri))
+            asset = AssetFactory.dynamic_create_instance_from_uri(type_uri)
+            created_assets.append(asset)
+
+            for attr in mapping[type_uri]['attributen']:
+                if attr['value'] is not None:
+                    value = attr['value']
+                    if attr['type'] == 'http://www.w3.org/2001/XMLSchema#decimal':
+                        value = float(attr['value'])
+
+                    DotnotationHelper.set_attribute_by_dotnotation(asset, dotnotation=attr['dotnotation'],
+                                                                   waarde_shortcut_applicable=True, value=value)
+                elif attr['range'] is not None:
+                    asset_atr = DotnotationHelper.get_attributes_by_dotnotation(asset, dotnotation=attr['dotnotation'],
+                                                                                waarde_shortcut_applicable=True)
+                    field = asset_atr.field
+
+                    if field == FloatOrDecimalField:
+                        asset_atr.field = self.create_extended_field_floatordecimal(field, attr['range'])
+                    else:
+                        raise NotImplementedError(f'Not implemented for {field}')
+
+
+
+                    pass
 
         return created_assets
+
+    def create_extended_field_floatordecimal(self, field, range_str: str):
+        extended_field = type('Extended' + field.__name__, (field, object), field.__dict__.copy())
+        extended_field.super_class = field
+
+        conditions = self.split_numeric_range_str(range_str)
+
+        def validate(value, attribuut, cls=extended_field):
+            s = cls.super_class
+            base_validate = s.validate(value, attribuut)
+            if not base_validate:
+                return base_validate
+
+            if len(conditions) == 1:
+                if conditions[0][0] == 'st':
+                    if value < conditions[0][1]:
+                        return True
+                    return False
+                elif conditions[0][0] == 'ste':
+                    if value <= conditions[0][1]:
+                        return True
+                    return False
+                elif conditions[0][0] == 'gt':
+                    if value > conditions[0][1]:
+                        return True
+                    return False
+                elif conditions[0][0] == 'gte':
+                    if value >= conditions[0][1]:
+                        return True
+                    return False
+            elif len(conditions) == 2:
+                if conditions[0][0] == 'gt' and conditions[1][0] == 'st':
+                    if conditions[0][1] < value < conditions[1][1]:
+                        return True
+                    return False
+                elif conditions[0][0] == 'gte' and conditions[1][0] == 'st':
+                    if conditions[0][1] <= value < conditions[1][1]:
+                        return True
+                    return False
+                elif conditions[0][0] == 'gt' and conditions[1][0] == 'ste':
+                    if conditions[0][1] < value <= conditions[1][1]:
+                        return True
+                    return False
+                elif conditions[0][0] == 'gte' and conditions[1][0] == 'ste':
+                    if conditions[0][1] <= value <= conditions[1][1]:
+                        return True
+                    return False
+
+        extended_field.validate = validate
+        return extended_field
 
     @staticmethod
     def load_postenmapping(posten_mapping_path: Path):
         reader = SQLDbReader(posten_mapping_path)
         data = reader.performReadQuery(
-            'SELECT standaardpostnummer, typeURI, attribuutURI, dotnotatie, defaultWaarde, bereik, UsageNote, isMeetstaatAttr, isAltijdInTeVullen, isBasisMapping, mappingStatus, mappingOpmerking FROM "MappingSB250"',
+            """SELECT standaardpostnummer, typeURI, attribuutURI, dotnotatie, dtAttriURI, defaultWaarde, bereik, 
+            usageNote, isMeetstaatAttr, isAltijdInTeVullen, isBasisMapping, mappingStatus, mappingOpmerking 
+            FROM MappingSB250
+            WHERE isBasisMapping = '1'""",
             {})
 
         mapping_dict = {}
@@ -49,9 +124,46 @@ class PostAssetFactory:
             attr_uri = str(row[2])
             if attr_uri != 'None':
                 dotnot_str = str(row[3])
+                type_str = str(row[4])
+                waarde_str = str(row[5])
+                if waarde_str == 'None':
+                    waarde_str = None
+                bereik_str = str(row[6])
+                if bereik_str == 'None':
+                    bereik_str = None
+
                 mapping_dict[mapping_nr][type_uri]['attributen'].append({
                     'typeURI': attr_uri,
-                    'dotnotation': dotnot_str
+                    'dotnotation': dotnot_str,
+                    'type': type_str,
+                    'value': waarde_str,
+                    'range': bereik_str
                 })
 
         return mapping_dict
+
+    @staticmethod
+    def split_numeric_range_str(range_str: str) -> []:
+        if range_str == '' or range_str is None or 'x' not in range_str:
+            return []
+
+        splitted = range_str.split(' ')
+        conditions = []
+
+        if splitted[2] == 'x':
+            if splitted[1] == '<':
+                conditions.append(('gt', float(splitted[0])))
+            elif splitted[1] == '<=':
+                conditions.append(('gte', float(splitted[0])))
+            if len(splitted) > 3:
+                if splitted[3] == '<':
+                    conditions.append(('st', float(splitted[4])))
+                elif splitted[3] == '<=':
+                    conditions.append(('ste', float(splitted[4])))
+        elif splitted[0] == 'x':
+            if splitted[1] == '<':
+                conditions.append(('st', float(splitted[2])))
+            elif splitted[1] == '<=':
+                conditions.append(('ste', float(splitted[2])))
+
+        return conditions
