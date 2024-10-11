9823e8e289c9cbbbf54870ab1a6e0b003db3d181
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-10-11 16:05:27 +0200

    updated

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from pathlib import Path
 
 from otlmow_converter.OtlmowConverter import OtlmowConverter
-from otlmow_model.OtlmowModel.Classes.Onderdeel.WVLichtmast import WVLichtmast
 
 from otlmow_postenmapping.PostAssetFactory import PostAssetFactory
+from pysize import get_size
 
 if __name__ == '__main__':
-    this_file = Path(__file__)
-    f = PostAssetFactory(this_file.parent / 'templates-Mapping_Artefact-versie kris 0.1.db', directory=this_file.parent)
+    this_directory = Path(__file__).parent
+    f = PostAssetFactory(this_directory / 'templates-Mapping_Artefact-versie kris 0.1.db', directory=this_directory)
 
-    base_assets = []
-    for i in range(5):
-        base_asset = WVLichtmast()
-        base_asset.assetId.identificator = f'000{i}'
-        base_asset.bestekPostNummer = ['WVlichtmast_config1', 'random bestekpostnummer']
-        base_assets.append(base_asset)
+    start_assets = OtlmowConverter().from_file_to_objects(file_path=this_directory / 'start_bestand.xlsx')
 
     template_created_assets = []
-    for i, asset in enumerate(base_assets):
+    for i, asset in enumerate(start_assets):
         template_created_assets.extend(f.create_assets_from_template(
             'WVlichtmast_config1', asset, unique_index=i))
 
     print(template_created_assets)
     OtlmowConverter().from_objects_to_file(sequence_of_objects=template_created_assets,
-                                           file_path=this_file.parent / 'output.xlsx')
+                                           file_path=this_directory / 'output.xlsx')
