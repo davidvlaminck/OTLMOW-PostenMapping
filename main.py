bc6fe6943073e2562749becbcbcffe784f7283be
VerdoodtDries <dries@nordend.eu>
2024-11-13 09:57:49 +0100

    Add parameter to function create_assets_from_mapping

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from otlmow_postenmapping.PostAssetFactory import PostAssetFactory
 
 if __name__ == '__main__':
     this_directory = Path(__file__).parent
-    f = PostAssetFactory(this_directory / 'templates-Mapping_Artefact-versie kris 0.1.db', directory=this_directory)
+    f = PostAssetFactory(this_directory / 'templates-Mapping_Artefact-versie kris 0.1.db', directory=this_directory,
+                         mapping_name='templates_mapping_artefact_krisVDS')
 
     start_assets = OtlmowConverter().from_file_to_objects(file_path=this_directory / 'start_bestand.xlsx')
 
     template_created_assets = []
     for i, asset in enumerate(start_assets):
         template_created_assets.extend(f.create_assets_from_mapping(
-            asset, unique_index=i))
+            asset, unique_index=i, keep_original_attributes=True))
 
     print(template_created_assets)
     OtlmowConverter().from_objects_to_file(sequence_of_objects=template_created_assets,
