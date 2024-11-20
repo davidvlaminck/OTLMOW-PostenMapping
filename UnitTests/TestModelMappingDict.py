368acd7aef7ecba7823fabf3ea54b6e5105fad0f
VerdoodtDries <dries@nordend.eu>
2024-11-20 09:22:32 +0100

    Extended dictionary with attribute types

--- UnitTests/TestModelMappingDict.py
+++ UnitTests/TestModelMappingDict.py
@@ -13,6 +13,114 @@ class PostenMappingDict:
                     "range": None,
                     "union_type_criterium": None
                 }
+                ,
+                "testIntegerField": {
+                    "typeURI": "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass.testIntegerField",
+                    "dotnotation": "testIntegerField",
+                    "type": "",
+                    "value": "9",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testStringField": {
+                    "typeURI": "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass.testStringField",
+                    "dotnotation": "testStringField",
+                    "type": "",
+                    "value": "myDummyString",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testDateField": {
+                    "typeURI": "",
+                    "dotnotation": "testDateField",
+                    "type": "",
+                    "value": "2000-01-01",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testDateTimeField": {
+                    "typeURI": "",
+                    "dotnotation": "testDateTimeField",
+                    "type": "",
+                    "value": "2000-01-01 01:01:01",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testKwantWrd": {
+                    "typeURI": "",
+                    "dotnotation": "testKwantWrd",
+                    "type": "",
+                    "value": "1.1",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testKeuzelijst": {
+                    "typeURI": "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass.testKeuzelijst",
+                    "dotnotation": "testKeuzelijst",
+                    "type": "",
+                    "value": "waarde-1",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                # ,
+                # "testIntegerFieldMetKard": {
+                #     "typeURI": "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass.testIntegerFieldMetKard",
+                #     "dotnotation": "testIntegerFieldMetKard",
+                #     "type": "",
+                #     "value": "1|2",  ## Dit is de juiste syntax
+                #     "range": None,
+                #     "union_type_criterium": None
+                # }
+                # ,
+                # "testStringFieldMetKard": {
+                #     "typeURI": "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass.testStringFieldMetKard",
+                #     "dotnotation": "testStringFieldMetKard",
+                #     "type": "",
+                #     "value": "myDummyString1|myDummyString2",
+                #     "range": None,
+                #     "union_type_criterium": None
+                # }
+                ,
+                "testComplexType.testStringField": {
+                    "typeURI": "",
+                    "dotnotation": "testComplexType.testStringField",
+                    "type": "",
+                    "value": "myDummyString",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testComplexType.testBooleanField": {
+                    "typeURI": "",
+                    "dotnotation": "testComplexType.testBooleanField",
+                    "type": "",
+                    "value": "True",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testUnionType.unionString": {
+                    "typeURI": "https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#DtuTestUnionType.unionString",
+                    "dotnotation": "testUnionType.unionString",
+                    "type": "",
+                    "value": "myDummyString",
+                    "range": None,
+                    "union_type_criterium": None
+                }
+                ,
+                "testUnionType.unionKwantWrd": {
+                    "typeURI": "https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#DtuTestUnionType.unionKwantWrd",
+                    "dotnotation": "testUnionType.unionKwantWrd",
+                    "type": "",
+                    "value": "1.1",
+                    "range": None,
+                    "union_type_criterium": None
+                }
             },
             "isHoofdAsset": True
         }
