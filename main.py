d77b08cbf6a99adf91fa721815d8af021f2b69de
VerdoodtDries <dries@nordend.eu>
2024-11-27 09:51:01 +0100

    change function call "create_assets_from_mapping_and_write_to_file".

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
 
     start_assets = OtlmowConverter().from_file_to_objects(file_path=this_directory / 'start_bestand.xlsx')
 
-    f.create_assets_from_mapping_and_write_to_file(
-        start_assets=start_assets,
-        output_directory=this_directory,
-        output_file_name='outputDries'
-    )
+    f.create_assets_from_mapping_and_write_to_file(start_assets=start_assets,
+                                                   output_directory=this_directory,
+                                                   output_file_name='outputDries',
+                                                   keep_original_attributes=True)
