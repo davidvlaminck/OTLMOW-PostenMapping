3d01c76e7e580116782f1fca6ada03d0981de94f
VerdoodtDries <dries@nordend.eu>
2024-11-29 18:06:52 +0100

    Update parameter output_path from function create_assets_from_mapping_and_write_to_file

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from otlmow_postenmapping.PostAssetFactory import PostAssetFactory
 
 if __name__ == '__main__':
     this_directory = Path(__file__).parent
-    f = PostAssetFactory(this_directory / 'templates-Mapping_Artefact-versie kris 0.1.db', directory=this_directory,
-                         mapping_name='templates_mapping_artefact_krisVDS')
+    f = PostAssetFactory(this_directory / 'templates-Mapping_Artefact-versie kris 0.1.db',
+                         directory=this_directory,
+                         mapping_name='templates_mapping_artefact_krisVDS'
+                         )
 
     start_assets = OtlmowConverter().from_file_to_objects(file_path=this_directory / 'start_bestand.xlsx')
 
     # TODO 2 parameters output_directory en output_file_name samenvoegen naar 1 parameter output_path
     f.create_assets_from_mapping_and_write_to_file(start_assets=start_assets,
-                                                   output_directory=this_directory,
-                                                   output_file_name='outputDries',
+                                                   output_path=this_directory / 'output.xlsx',
                                                    keep_original_attributes=True,
                                                    append_all_attributes=True
                                                    )
